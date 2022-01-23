d246afd128d4a7af9627571be854b90bb98413e8
VoitehovichXenia <voitehovichkg@gmail.com>
2022-01-23 10:24:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const MORSE_TABLE = {
-    '.-':     'a',
+    '.-':     'a', 
     '-...':   'b',
     '-.-.':   'c',
     '-..':    'd',
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let decoded = expr.split('**********');
+
+    decoded = decoded.map( (word) => {
+        let newWord = '';
+
+        for (let i = 0; i < word.length; i = i + 10) {
+            let morse = '';
+            let letter = word.slice(i, i+10);
+
+            for (let k = letter.length-1; k >= 0; k = k - 2) {
+            
+                if ((letter[k-1] + letter[k]) === '11') {
+                    morse = '-' + morse;
+                }
+                if ((letter[k-1] + letter[k]) === '10') {
+                    morse = '.' + morse;
+                }      
+            }
+
+            newWord += MORSE_TABLE[morse];
+        }
+        
+        return newWord;
+    });
+        
+    return decoded.join(' ');
 }
 
 module.exports = {
